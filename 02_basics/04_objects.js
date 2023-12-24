74626a0b83d801ea153b0cb2ce27870b09f5b03b
Aditya Mohaty <129523669+aditya2445@users.noreply.github.com>
2023-12-24 20:18:42 +0000

    Read a little bit about JSON

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -25,7 +25,7 @@ const obj2={3:"a",4:"b"};
 //{} is optional go to object.assign in mdn docs actually assign has(target,source) i.e it copies all the sources into the target if you dont give {} and write obj1,obj2 then target is now obj1 and everything will get copied into obj1 thats why we are giving an empty object which acts as a target.
 
 const obj3={...obj1,...obj2}//most used way to join
-console.log(obj3);
+// console.log(obj3);
 
 //from database
 
@@ -44,11 +44,37 @@ const users=[
     }
 ]
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));//return an array of keys
-console.log(Object.values(tinderUser));//return an array of values of keys
-console.log(Object.entries(tinderUser));//return an array of [key ,value]
+// console.log(Object.keys(tinderUser));//return an array of keys
+// console.log(Object.values(tinderUser));//return an array of values of keys
+// console.log(Object.entries(tinderUser));//return an array of [key ,value]
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));//true
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));//true
 //Assignment go through some important methods of object by typing the object in console of browser and read the prototype list
+
+const course={
+    coursename:"js in hindi",
+    price:"$885",
+    courseInstructor:"hitesh"
+}
+
+const {courseInstructor:instructor}=course //destructuring 
+console.log(instructor);//hitesh
+
+//later in react we will read
+// const navbar=({company})=>{
+
+// }
+// navbar(company="hitesh");
+
+// example of an api:https://api.github.com/users/aditya2445
+
+
+
+
+
+
+
+
+
