939b20b3a7cab41bd6b077783c181c624b55e564
Aditya Mohaty <129523669+aditya2445@users.noreply.github.com>
2023-12-24 14:22:30 +0000

    Stack and Heap memory discussed in this part

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -16,10 +16,10 @@ let userEmail=undefined;
 const id=Symbol('123')
 const anotherId=Symbol('123')
 
-console.log(id);//Symbol(123)
-console.log(anotherId);//Symbol(123)
-console.log(id==anotherId);//false
-console.log(id===anotherId);//false
+// console.log(id);//Symbol(123)
+// console.log(anotherId);//Symbol(123)
+// console.log(id==anotherId);//false
+// console.log(id===anotherId);//false
 
 const bigNumber=3354636377878797773n//BigInt
 
@@ -39,7 +39,30 @@ const myFunction=function(){
     console.log("Hello Function");
 }
 
-console.log(typeof(bigNumber));
+// console.log(typeof(bigNumber));
 
 //NOTE : NULL is an object type
 //Assignment:https://262.ecma-international.org/5.1/#sec-11.4.3...
+
+
+// ****************************************************************
+//Stack(primitive)  Heap (non primitive)
+
+let myYtName="Aditya.com"
+
+let anotherName="AdityaVlogsDaily"
+anotherName="Adityaaaa"//a copy is created and any changes in the variable will reflect in the copy notr in the original ones 
+
+console.log(anotherName);
+console.log(myYtName);
+
+let user1={
+    email:"user@123.com",
+    upi:"user@ybl"
+}
+
+let user2=user1;//actual user1 is passed now changes in user2 will reflect in the user1 also
+
+user2.email="adi@goog.com"
+console.log(user1.email);//Both are same i.e, adi@goog.com
+console.log(user2.email);
