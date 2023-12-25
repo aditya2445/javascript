9dbc4c8228de507ef56792aad508fb2449717670
Aditya Mohaty <129523669+aditya2445@users.noreply.github.com>
2023-12-25 03:24:50 +0000

    Array and object passing in function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -3,7 +3,7 @@
 // }
 
     function addTwoNumbers(number1,number2){//parameters
-        console.log(number1+number2);
+        // console.log(number1+number2);
     }
 
 // addTwoNumbers(3,4);//arguements o/p->7
@@ -16,6 +16,33 @@ function loginUserMessage(username){
 
 const message=loginUserMessage("Aditya");
 const message2=loginUserMessage();
-console.log(message);//Aditya just logged in
-console.log(message2);//undefined just logged in
+// console.log(message);//Aditya just logged in
+// console.log(message2);//undefined just logged in
 //if we give some default value in function defination then overriding happens if we pass some explicit parameters or else it will print with the default ones
+
+function calculateCardPrice(...num1){//... is rest operator and its the spread operator
+    return num1
+}
+
+// console.log(calculateCardPrice(200,300,500));//o/p->[200,300,500]
+
+const obj={
+    name:"Aditya",
+    price:"199"
+}
+
+function objectHandler(anyObj) {
+    // console.log(`username is ${anyObj.name} and price is ${anyObj.price}`);//this operator doesnot works here
+}
+
+// objectHandler(obj) method 1 of passig object
+// objectHandler({name:"sam",price:"222"});//method 2 of passing an object
+
+
+const arr=[100,200,300]
+
+function returnSecondValue(getarr){
+    return getarr[1]
+}
+
+console.log(returnSecondValue(arr));//200
